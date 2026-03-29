846cf7b69ad8ce2ff05e8e08ecf2c639f2211fb1
Dogibodogi <sacaleanu43@gmail.com>
2026-03-29 18:53:59 +0300

    feat: planet spinning and pause menu

---
+++ I Beg You, Do Nothing (Moon Edition)/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem; // Adăugăm asta pentru sistemul nou
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenuPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Așa se scrie "GetKeyDown" în sistemul nou
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
